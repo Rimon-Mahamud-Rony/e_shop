6b6c3f9bbe9d79a02cf6d1b3880e00654a8822f2
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2023-07-28 08:50:12 +0600

    commit 1

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="card">
+	<div class="card-body">
+		<h1>Template added</h1>
+	</div>
+</div>
+
+<div class="container-fluid">
+		<h1>Finally Templated added successfully</h1>
+</div>
+
+
+@endsection
